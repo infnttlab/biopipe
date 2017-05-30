4775c6b700628ff5b98cc610e3bf0bbdbdbe0b16
EnricoGiampieri <enrico.giampieri@unibo.it>
2017-05-30 13:56:19 +0200

    Fixed path problem

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod ugo+x Miniconda3-latest-Linux-x86_64.sh 
 ./Miniconda3-latest-Linux-x86_64.sh
-source .bashrc 
+source ~/.bashrc 
+PATH=~/miniconda3/bin:$PATH
+export PATH 
 pip install -U snakemake
 conda config --add channels bioconda
 
