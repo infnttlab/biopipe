de28952f18bb5694e189e7f52f37f51fab1ad0bd
Draco95 <daniele.dallolio95@gmail.com>
2017-06-01 14:30:27 +0200

    Added psutil in installer

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod ugo+x Miniconda3-latest-Linux-x86_64.sh 
-./Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh -b
 source ~/.bashrc 
 PATH=~/miniconda3/bin:$PATH
-export PATH 
+export PATH
+conda update conda -y
+pip install -U psutil
 pip install -U snakemake
 pip install -U PyYAML
 conda config --add channels bioconda
