65402e24d27d4e2c3e22efc07343a9dd2d84ba02
Draco95 <daniele.dallolio95@gmail.com>
2017-08-01 11:56:55 +0200

    Edited split

--- split.py
+++ split.py
@@ -1,17 +1,17 @@
 import itertools as it
 
-n_times = 5
+n_times = 1
 
 start = 0
-range_width = 100000
+range_width = 5000
 
 for i in range(1, n_times+1):
     nreads = i * range_width
     finish = nreads + start
-    origin_1 = "~/data_ref/SRR1611178_1.fastq"
-    origin_2 = "~/data_ref/SRR1611178_2.fastq"
-    target_1 = "~/biopipe/data/subset_{start}_{finish}_1.fastq".format(start=start,finish=finish)
-    target_2 = "~/biopipe/data/subset_{start}_{finish}_2.fastq".format(start=start,finish=finish)
+    origin_1 = "/home/daniele/Scrivania/sequence_data/SRR1611178_1.fastq"
+    origin_2 = "/home/daniele/Scrivania/sequence_data/SRR1611178_2.fastq"
+    target_1 = "subset_{start}_{finish}_1.fastq".format(start=start,finish=finish)
+    target_2 = "subset_{start}_{finish}_2.fastq".format(start=start,finish=finish)
     
     files = [(origin_1,target_1),(origin_2,target_2)]
     
