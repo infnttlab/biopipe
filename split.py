d2c381d6a1c3761ba7903b953803c0e9eada5be3
Draco95 <daniele.dallolio95@gmail.com>
2017-08-01 15:56:17 +0200

    added params in split

--- split.py
+++ split.py
@@ -1,30 +1,28 @@
 import itertools as it
+import sys
 
-n_times = 1
+start = sys.argv[1]
+finish = sys.argv[2]
 
-start = 0
-range_width = 5000
+origin_1 = "/home/daniele/Scrivania/sequence_data/SRR1611178_1.fastq"
+origin_2 = "/home/daniele/Scrivania/sequence_data/SRR1611178_2.fastq"
 
-for i in range(1, n_times+1):
-    nreads = i * range_width
-    finish = nreads + start
-    origin_1 = "/home/ddallolio/data_ref/SRR1611178_1.fastq"
-    origin_2 = "/home/ddallolio/data_ref/SRR1611178_2.fastq"
-    target_1 = "./"+"data/subset_{start}_{finish}_1.fastq".format(start=start,finish=finish)
-    target_2 = "./"+"data/subset_{start}_{finish}_2.fastq".format(start=start,finish=finish)
-    
-    files = [(origin_1,target_1),(origin_2,target_2)]
-    
-    for origin, target in files:
-        with open(origin, 'r') as origin_file:
-            with open(target, 'w') as target_file:
-                size = 4
-                begin = start*size
-                end = (start+nreads)*size
-                partial_lines = it.islice(origin_file, begin, end)
-                for line in partial_lines:
-                    stripped_line = line.strip()
-                    print(stripped_line, file=target_file)
+#origin_1 = "/home/ddallolio/data_ref/SRR1611178_1.fastq"
+#origin_2 = "/home/ddallolio/data_ref/SRR1611178_2.fastq"
+target_1 = "./subset_{start}_{finish}_1.fastq".format(start=start,finish=finish)
+target_2 = "./subset_{start}_{finish}_2.fastq".format(start=start,finish=finish)
+
+files = [(origin_1,target_1),(origin_2,target_2)]
+
+for (origin, target) in files:
+    with open(origin, 'r') as origin_file:
+        with open(target, 'w') as target_file:
+            begin = int(start)*4
+            end = int(finish)*4
+            partial_lines = it.islice(origin_file, begin, end)
+            for line in partial_lines:
+                stripped_line = line.strip()
+                print(stripped_line, file=target_file)
             
 # %%
         
