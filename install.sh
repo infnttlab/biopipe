55f879c6f58b6eb992cca0580f7c3c757e4fe400
Draco95 <daniele.dallolio95@gmail.com>
2017-08-04 15:08:56 +0200

    Added pandas, ipython and fixed bugs

--- install.sh
+++ install.sh
@@ -2,17 +2,21 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod ugo+x Miniconda3-latest-Linux-x86_64.sh 
 bash ./Miniconda3-latest-Linux-x86_64.sh -b
-source ~/.bashrc 
-PATH=~/miniconda3/bin:$PATH
-export PATH
+echo " " >> ~/.bashrc
+echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.bashrc
 conda update conda -y
 pip install -U psutil
 pip install -U snakemake
 pip install -U PyYAML
 pip install matplotlib
+pip install pandas
+pip install ipython
 conda config --add channels bioconda
 mkdir ~/data_ref
 scp /mnt/avoton/biofisici/data_snakemake/human_g1k_v37.fasta.gz ~/data_ref/
 scp /mnt/avoton/biofisici/data_snakemake/SRR1611178_1.fastq.gz ~/data_ref/
 scp /mnt/avoton/biofisici/data_snakemake/SRR1611178_2.fastq.gz ~/data_ref/
 gunzip ~/data_ref/*.gz
+rm ~/biopipe/Miniconda3-latest-Linux-x86_64.sh
+chmod ugo+x script.sh
+rm ./Miniconda3-latest-Linux-x86_64.sh
